bb4c1ea77351cf0a24d301faf2f5f7ad87ff3d53
xSioNe <edik.grekov@gmail.com>
2023-02-18 16:54:27 +1100

    dog.app(1.0.0.0)

--- js/main(cat).js
+++ js/main(cat).js
@@ -1,52 +1,9 @@
 const doc = document;
 const con = console.log;
-const img = doc.querySelector('img');
-const newImgBtn = doc.querySelector('button');
-const urlRandom = 'https://dog.ceo/api/breeds/image/random';
-const urlAll = 'https://dog.ceo/api/breeds/list/all';
-// const urlCustom = 'https://dog.ceo/api/breed/Caucasian Ovcharka/images/random';
-const urlCustom = 'https://dog.ceo/api/breed/';
-const selectDog = doc.querySelector('.selectDog');
-let thisDog = 'random dog';
-let rDog = '';
+const url = 'https://api.thecatapi.com/v1/images/search';
 
-
-
-fetch(urlAll)
-        .then(response => response.json())
-        .then(data => {
-            const breeds = data.message;
-            for (let breed in breeds) {
-                const cBreed = breeds[breed];
-                if (!cBreed.length) {
-                    rDog += `<option value="${breed}">${breed}</option>`
-                    continue
-                }
-                cBreed.forEach(item => {
-                    rDog += `<option value="${item} ${breed}">${item} ${breed}</option>`
-                });
-            }
-            selectDog.innerHTML += rDog;
-            selectDog.onchange = function() {
-                thisDog = this.value;
-            }
-            newImgBtn.onclick = renderImg;
-        });
-
-
-
-// https://dog.ceo/dog-api/breeds-list
-
-function renderImg() {
-    let urlDog = `${urlCustom}${thisDog}/images/random`;
-    if (thisDog == 'random dog') {
-        urlDog = urlRandom;
-    }
-    con(urlDog)
-    fetch(urlDog)
-        .then(response => response.json())
-        .then(data => {
-            const src = data.message;
-            img.src = src;
-        });
-}
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let img = data[0].url;
+    })
